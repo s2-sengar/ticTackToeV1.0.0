7f8a3e61aaed30e2ad0c3b104bc0613c6c781c13
Sudhanshu <sudhanshu.iem2k18@gmail.com>
2021-05-19 11:52:52 +0530

    Layout

---
+++ app/src/main/java/com/s2/sengar/tic_tac_toe_v10/ticTacToeMain.kt
@@ -0,0 +1,11 @@
+package com.s2.sengar.tic_tac_toe_v10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ticTacToeMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tic_tac_toe_main)
+    }
+}
